efe86bd7a2e9bb508309fc296d12a3109a8bfff1
Jaco Kloppers <jaco.kloppers@gmail.com>
2020-04-28 09:25:45 +0200

    Created reactive form to type in location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { RouterModule } from '@angular/router';
 import { allAppRoutes } from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, WeatherComponent],
-  imports: [BrowserModule, RouterModule.forRoot(allAppRoutes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
